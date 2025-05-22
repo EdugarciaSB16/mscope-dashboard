f32f256fc790d6e2747e340b7071339d83492091
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-22 21:35:54 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="text-3xl underline text-blue-400">
+      <div>Dashboard</div>
+    </div>
+  );
+};
+
+export default App;
