2f51a022ba3331149eeb58b8263e850faa12889f
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-24 00:42:32 +0200

    refactor layouts and containers

--- src/features/company/CompanyHeader.tsx
+++ src/features/company/CompanyHeader.tsx
@@ -6,16 +6,16 @@ import CompanyRadar from './CompanyRadar';
 const CompanyHeader = () => {
   return (
     <div className="w-full">
-      <Card className="bg-white shadow-sm">
-        <CardContent className="p-0">
-          <div className="grid grid-cols-1 lg:grid-cols-4 divide-y lg:divide-y-0 lg:divide-x divide-zinc-200 h-full">
-            <div className="lg:col-span-2 p-6 h-full">
+      <Card className="bg-white shadow-sm h-full">
+        <CardContent className="p-0 h-full">
+          <div className="grid grid-cols-1 lg:grid-cols-4 divide-y lg:divide-y-0 lg:divide-x divide-zinc-200 h-full min-h-[350px]">
+            <div className="lg:col-span-2 p-6 flex flex-col h-full">
               <CompanyDetails />
             </div>
-            <div className="lg:col-span-1 p-6 h-full">
+            <div className="lg:col-span-1 p-6 flex flex-col h-full">
               <CompanyFinancials />
             </div>
-            <div className="lg:col-span-1 p-6 h-full">
+            <div className="lg:col-span-1 p-6 flex flex-col h-full">
               <CompanyRadar />
             </div>
           </div>
