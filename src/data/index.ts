1019003205c039754ff7a4e5296c74e7d087c3f4
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-24 00:42:19 +0200

    add mockdata

--- src/data/index.ts
+++ src/data/index.ts
@@ -25,3 +25,30 @@ export const navigationLinks = [
     isBeta: true,
   },
 ];
+
+export const company = {
+  name: 'Company name',
+  description:
+    'Company specializes in the development and manufacturing of weaving machines and technology. Known for its advanced and efficient looms, Company integrates cutting-edge technology in its products to enhance textile production capabilities.',
+  cif: 'B08245671',
+  location: 'Madrid, Spain',
+  founded: 2022,
+  website: 'www.companyname.es',
+  microsector: 'High growth SME index',
+  score: 'A1',
+  turnover: 30000000,
+  growthCAGR: 6.1,
+  ebitda: 5000000,
+  ebitdaCAGR: 4.3,
+  ebitdaMargin: 8.5,
+  nfdToEbitda: 2.4,
+  indicators: {
+    Growth: 4,
+    Size: 3,
+    Margins: 4,
+    Sharpe: 2,
+    Balance: 3,
+    Autonomy: 5,
+    Profit: 4,
+  },
+};
