0087109a3b5453f758ef12072e208ad9a6ec4439
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-25 21:39:00 +0200

    refactor style CompanyTab

--- src/features/company/CompanyTab.tsx
+++ src/features/company/CompanyTab.tsx
@@ -11,24 +11,30 @@ const CompanyTab: React.FC = () => {
   ];
 
   return (
-    <Tabs defaultValue="financials" className="w-fit">
-      <TabsList className="bg-[#e6e6e6] p-0 h-auto overflow-x-auto max-w-[calc(100vw-32px)] sm:max-w-none flex flex-nowrap">
-        {tabItems.map((tab) => (
-          <TabsTrigger
-            key={tab.id}
-            value={tab.id}
-            className="px-2 sm:px-4 py-1 sm:py-2 rounded whitespace-nowrap
-            data-[state=active]:border-black
-            data-[state=active]:shadow-[0px_0px_4px_#1d1d1d40] 
-            data-[state=inactive]:border-none"
-          >
-            <span className="text-[11px] sm:text-[12px] font-[500] tracking-[0px] leading-[16px]">
-              {tab.label}
-            </span>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-    </Tabs>
+    <div className="px-2">
+      <Tabs defaultValue="summary" className="w-fit">
+        <TabsList className="bg-[#e6e6e6] rounded-[4px] p-[2px] h-auto overflow-x-auto max-w-[calc(100vw-48px)] sm:max-w-none flex flex-nowrap">
+          {tabItems.map((tab) => (
+            <TabsTrigger
+              key={tab.id}
+              value={tab.id}
+              className="px-2 sm:px-2 py-1 rounded-[4px] whitespace-nowrap 
+                data-[state=active]:bg-white
+                data-[state=active]:text-black
+                data-[state=active]:border-[#626266]
+                data-[state=inactive]:border-none
+                data-[state=inactive]:hover:bg-white/50 transition-colors 
+                data-[state=inactive]:text-[#626266]
+                "
+            >
+              <span className="text-[10px] sm:text-[12px] tracking-[0px] leading-[16px]">
+                {tab.label}
+              </span>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+    </div>
   );
 };
 
