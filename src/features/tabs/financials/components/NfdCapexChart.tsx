42e955e503bd456a8be26579345d9ed33a25644e
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-26 20:09:44 +0200

    fix data NfdCapexChart component

--- src/features/tabs/financials/components/NfdCapexChart.tsx
+++ src/features/tabs/financials/components/NfdCapexChart.tsx
@@ -10,17 +10,17 @@ import {
 } from 'recharts';
 
 const data = [
-  { year: '2012', nfdEbitda: 2.4, capexEbitda: 0.8 },
-  { year: '2013', nfdEbitda: 2.6, capexEbitda: 0.95 },
-  { year: '2014', nfdEbitda: 3.5, capexEbitda: 0.82 },
-  { year: '2015', nfdEbitda: 2.3, capexEbitda: 0.78 },
-  { year: '2016', nfdEbitda: 2.4, capexEbitda: 0.8 },
-  { year: '2017', nfdEbitda: 2.0, capexEbitda: 0.82 },
-  { year: '2018', nfdEbitda: 2.8, capexEbitda: 0.85 },
-  { year: '2019', nfdEbitda: 2.7, capexEbitda: 0.88 },
-  { year: '2020', nfdEbitda: 3.0, capexEbitda: 1.1 },
-  { year: '2021', nfdEbitda: 3.5, capexEbitda: 0.87 },
-  { year: '2022', nfdEbitda: 2.6, capexEbitda: 0.85 },
+  { year: '2012', nfdEbitda: 0.8, capexEbitda: 2.4 },
+  { year: '2013', nfdEbitda: 0.95, capexEbitda: 2.6 },
+  { year: '2014', nfdEbitda: 0.82, capexEbitda: 3.5 },
+  { year: '2015', nfdEbitda: 0.78, capexEbitda: 2.3 },
+  { year: '2016', nfdEbitda: 0.8, capexEbitda: 2.4 },
+  { year: '2017', nfdEbitda: 0.82, capexEbitda: 2.0 },
+  { year: '2018', nfdEbitda: 0.85, capexEbitda: 2.8 },
+  { year: '2019', nfdEbitda: 0.88, capexEbitda: 2.7 },
+  { year: '2020', nfdEbitda: 1.1, capexEbitda: 3.0 },
+  { year: '2021', nfdEbitda: 0.87, capexEbitda: 3.5 },
+  { year: '2022', nfdEbitda: 0.85, capexEbitda: 2.6 },
 ];
 
 const NfdCapexChart = () => {
@@ -55,7 +55,7 @@ const NfdCapexChart = () => {
           type="monotone"
           dataKey="nfdEbitda"
           name="NFD/EBITDA"
-          stroke="#58585A"
+          stroke="#D8D8D9"
           strokeWidth={2}
           dot={false}
         />
@@ -63,7 +63,7 @@ const NfdCapexChart = () => {
           type="monotone"
           dataKey="capexEbitda"
           name="CAPEX/EBITDA"
-          stroke="#D8D8D9"
+          stroke="#58585A"
           strokeWidth={2}
           dot={false}
         />
